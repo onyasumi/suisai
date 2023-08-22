9dabc02aa202d70977cfddfefff7a7d3171bbf37
onyasumi <boku@kaneki.dev>
2023-08-22 17:20:02 -0400

    created rename album

--- src/controllers/album.rs
+++ src/controllers/album.rs
@@ -7,6 +7,7 @@ use axum::TypedHeader;
 use axum::headers::Authorization;
 use axum::headers::authorization::Bearer;
 use surrealdb::sql::Thing;
+use crate::models::album::AlbumWrapper;
 
 
 #[debug_handler]
@@ -45,5 +46,36 @@ pub async fn create_album(TypedHeader(header): TypedHeader<Authorization<Bearer>
 
     crate::DB.invalidate();
     (StatusCode::OK, id)
+
+}
+
+
+#[debug_handler]
+pub async fn rename_album(TypedHeader(header): TypedHeader<Authorization<Bearer>>, Json(payload): Json<models::album::AlbumWrapper>) -> (StatusCode, String) {
+
+    // Authenticate with JWT & extract metadata
+    let user_id: Thing = match utils::auth::authenticate(header.token()).await {
+        Ok(t) => t,
+        Err(err) => return (StatusCode::UNAUTHORIZED, err.to_string())
+    };
+
+    match crate::DB.query("UPDATE $id SET label = $new_label")
+        .bind(("id", payload.id))
+        .bind(("new_label", payload.album.label)).await {
+            Ok(_) => (),
+            Err(err) => return (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+    }
+
+    crate::DB.invalidate();
+
+    (StatusCode::OK, "meow".to_string())
+
+}
+
+
+#[debug_handler]
+pub async fn get_albums(TypedHeader(header): TypedHeader<Authorization<Bearer>>, Json(payload): Json<models::album::AlbumWrapper>) -> (StatusCode, Result<Json<Vec<models::album::AlbumWrapper>>, String>) {
+
+    (StatusCode::IM_A_TEAPOT, Err("meow".to_string()))
 
 }
